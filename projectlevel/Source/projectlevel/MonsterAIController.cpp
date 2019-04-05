3784222eb4c1a2fed6ffda9a58d9e5ed8dbd6ee4
hohyuk <ghgur91@gmail.com>
2019-04-05 17:42:34 +0900

    MonsterAI컨트롤러 제작중

--- projectlevel/Source/projectlevel/MonsterAIController.cpp
+++ projectlevel/Source/projectlevel/MonsterAIController.cpp
@@ -1,13 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MonsterAIController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+const FName AMonsterAIController::HomePosKey(TEXT("HomePos"));
+const FName AMonsterAIController::PatrolPoskey(TEXT("PatrolPos"));
+const FName AMonsterAIController::TargetKey(TEXT("Target"));
 
 AMonsterAIController::AMonsterAIController()
 {
+	static ConstructorHelpers::FObjectFinder<UBlackboardData> BBObject(TEXT("/Game/TowerofAngra/AI/MonsterBlackboard.MonsterBlackboard"));
+	if (BBObject.Succeeded())
+	{
+		BBAsset = BBObject.Object;
+	}
 
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BTObject(TEXT("/Game/TowerofAngra/AI/MonsterBehaviorTree.MonsterBehaviorTree"));
+	if (BTObject.Succeeded())
+	{
+		BTAsset = BTObject.Object;
+	}
 }
 
 void AMonsterAIController::Possess(APawn * InPawn)
 {
+	Super::Possess(InPawn);
+
+	if (UseBlackboard(BBAsset, Blackboard))
+	{
+		Blackboard->SetValueAsVector(HomePosKey, InPawn->GetActorLocation());
+	}
 }
--- projectlevel/Source/projectlevel/NormalMonster.cpp
+++ projectlevel/Source/projectlevel/NormalMonster.cpp
@@ -1,13 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "NormalMonster.h"
-
-
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "ConstructorHelpers.h"
 // Sets default values
 ANormalMonster::ANormalMonster()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 100.0f);
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MonsterMesh(TEXT("/Game/Mannequin/Character/Mesh/SK_Mannequin"));
+	GetMesh()->SetSkeletalMesh(MonsterMesh.Object);
+	GetMesh()->SetWorldLocation(FVector(0, 0, -100.f), false, NULL, ETeleportType::None);
+	GetMesh()->SetWorldRotation(FRotator(0, -90, 0), false, NULL, ETeleportType::None);
 
 }
 
