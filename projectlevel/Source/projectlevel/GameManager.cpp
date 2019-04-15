dc98b4da65d0e6ff1eb731a0474bbc11a344bfd5
mtkim <jumbak1234@gmail.com>
2019-04-15 22:45:14 +0900

    GameManager Monster Spawn Update

---
+++ projectlevel/Source/projectlevel/GameManager.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameManager.h"
+#include "Monster.h"
+#include "Engine/World.h"
+
+// Sets default values
+AGameManager::AGameManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGameManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGameManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//this is server's coding area
+	if (true)
+	{
+		return;
+	}
+}
+
+
+void AGameManager::SpawnMonster()
+{
+
+	UWorld* world = GetWorld();
+	////Spawn Actor with Static Class
+	//FActorSpawnParameters Sparam;
+	FActorSpawnParameters Sparam;
+	Sparam.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	Sparam.Owner = this;
+	Monster = world->SpawnActor<AMonster>(AMonster::StaticClass(), FVector(0, 0, 0), FRotator(0, 0, 0), Sparam);
+}
+
